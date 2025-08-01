fede1cdbe3fdd047ef355828c739916a376b7831
VeniaminKrutoi <mush091091091@gmail.com>
2025-08-01 12:08:23 +0300

    Initial Commit

---
+++ DailyQuiz/DailyQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuizApp.swift
+//  DailyQuiz
+//
+//  Created by Вениамин Гребенёв on 01.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyQuizTests/DailyQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuizTests.swift
+//  DailyQuizTests
+//
+//  Created by Вениамин Гребенёв on 01.08.2025.
+//
+
+import Testing
+@testable import DailyQuiz
+
+struct DailyQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
